725372df2f84c0b684e7f21f6384676cbd2a0023
NadiaBCarmon <nadia.prysmmagazine@gmail.com>
2019-01-23 19:18:26 -0500

    Code for the very basics of React Native. Including state, props, lifecycle methods, etc. Boilerplate.

---
+++ albums/index.js
@@ -0,0 +1,19 @@
+//Import a library to help create a component//
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+//Create a componenet//
+
+const App = () => (
+
+		<View>
+		<Header headerText={'Albums'} />
+		<AlbumList />
+		</View>		
+);
+
+
+//Render component to the device//
+AppRegistry.registerComponent('albums', () => App);
+
